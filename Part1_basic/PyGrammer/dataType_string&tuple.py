6a449e89aef3e5e763444c1079c343bff76af7fe
eunbin-hyun <eunbin8592@gmail.com>
2025-04-11 08:22:37 +0000

    pygrammer

---
+++ Part1_basic/PyGrammer/dataType_string&tuple.py
@@ -0,0 +1,28 @@
+#5강 문자열,튜플 자료형
+
+# #문자열
+data = 'Hello World'
+print(data)
+
+data = "Don't you know \"Python\"?"
+print(data)
+
+# #문자열 연산 (문자열은 변경불가)
+a = "Hello"
+b = "World"
+print(a + " " + b)
+
+a = "String"
+print(a * 3)
+
+a = "ABCDEF"
+print(a[2:4])
+
+
+# #튜플
+# #튜플은 리스트와 유사하지만, 변경 불가하고 소괄호 이용
+tuple_a = (1,2,3,4,5,6,7,8,9)
+print(tuple_a)
+print(tuple_a[3])
+print(tuple_a[1:4])
+
