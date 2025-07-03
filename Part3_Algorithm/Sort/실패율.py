34bd18e9b951819dac43b75e8a63e32a4984b094
eunbin-hyun <eunbin8592@gmail.com>
2025-07-03 11:45:02 +0000

    sort

---
+++ Part3_Algorithm/Sort/실패율.py
@@ -0,0 +1,23 @@
+# 정렬25: 실패율
+# 실패율 = 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+
+N = int(input())
+stages = list(map(int, input().split()))
+answer = []
+length = len(stages)
+
+for i in range(1,N+1):
+    count = stages.count(i)
+
+    # 실패율 계산 
+    if length == 0:
+        fail = 0
+    else:
+        fail = count / length
+
+    answer.append((i, fail))
+    length -= count
+
+answer = sorted(answer, key=lambda t: t[1], reverse=True)
+answer = [i[0] for i in answer]
+print(answer)
